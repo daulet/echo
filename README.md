c38dbb57280f94f53168c31ea41fe2e7bff2306d
Daulet Zhanguzin <daulet@zhanguzin.kz>
2022-07-11 23:02:55 -0700

    add benchmarks to compare to other implementations

--- README.md
+++ README.md
@@ -1,15 +1,43 @@
-# Punchbag
+# Echo
 
-Ever wanted to have something (or someone) to send your random TCP packets to? Punchbag to the rescue!
+A TCP server that will efficiently write back all received bytes.
 
-Use:
+## Benchmark
 
+`BenchmarkKnownCount` is the fastest benchmark, but there are other, more realistic, implementations available.
 ```
-go run app/main.go -port=9001
+go run app/main.go -port=1122
+go test -bench=BenchmarkKnownCount -benchmem -count=5
 ```
 
-Test:
-
+### Results
+Running in batches of 64KB:
 ```
-nc localhost 9001
+goos: linux
+goarch: amd64
+pkg: github.com/daulet/echo
+cpu: AMD Ryzen 9 5900HS with Radeon Graphics        
+BenchmarkKnownCount-16          1000000000               0.04737 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.06366 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.06375 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.05343 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.06283 ns/op         0 B/op          0 allocs/op
+PASS
+ok      github.com/daulet/echo  0.418s
+```
+
+### BPF-Echo
+[eBPF echo server](https://github.com/path-network/bpf-echo) implementation doesn't seem to be able to handle batch size above 16KB, but it seems to be a bit more efficient:
+```
+goos: linux
+goarch: amd64
+pkg: github.com/daulet/echo
+cpu: AMD Ryzen 9 5900HS with Radeon Graphics        
+BenchmarkKnownCount-16          1000000000               0.03328 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.02866 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.02907 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.03115 ns/op         0 B/op          0 allocs/op
+BenchmarkKnownCount-16          1000000000               0.03171 ns/op         0 B/op          0 allocs/op
+PASS
+ok      github.com/daulet/echo  0.224s
 ```
---
+++ tcp_test.go
@@ -0,0 +1,177 @@
+package echo_test
+
+import (
+	"net"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+const workers = 64
+
+func BenchmarkAdaptive(b *testing.B) {
+	// pushing this higher causes the benchmark not terminate
+	const batch = 2 * 1024
+
+	var conns []net.Conn
+	for i := 0; i < 1000; i++ {
+		conn, err := net.Dial("tcp", "127.0.0.1:1122")
+		if err != nil {
+			b.Error(err)
+			return
+		}
+		conns = append(conns, conn)
+	}
+	defer func() {
+		for _, conn := range conns {
+			conn.Close()
+		}
+	}()
+
+	var (
+		wg      sync.WaitGroup
+		ch      = make(chan []byte)
+		payload = make([]byte, batch)
+		maxGort = 0
+	)
+	for i := 0; i < b.N; i += batch {
+		select {
+		case ch <- payload:
+			continue
+		default:
+		}
+
+		if gort := runtime.NumGoroutine(); maxGort < gort {
+			maxGort = gort
+		}
+
+		wg.Add(1)
+		go func(conn net.Conn) {
+			defer wg.Done()
+
+			buf := make([]byte, batch)
+			for v := range ch {
+				_, err := conn.Write(v)
+				if err != nil {
+					b.Error(err)
+					break
+				}
+				receive := len(v)
+				for receive > 0 {
+					n, err := conn.Read(buf)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+					receive -= n
+				}
+				if receive < 0 {
+					b.Errorf("received %d extra bytes", -receive)
+				}
+			}
+		}(conns[i%len(conns)])
+
+		ch <- payload
+	}
+	close(ch)
+	wg.Wait()
+
+	b.Logf("max goroutines: %d\n", maxGort)
+}
+
+func BenchmarkKnownCount(b *testing.B) {
+	const batch = 64 * 1024
+
+	var wg sync.WaitGroup
+	for i := 0; i < workers; i++ {
+		count := b.N / workers
+		if i <= b.N%workers {
+			count++
+		}
+
+		wg.Add(1)
+		go func(count int) {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", "127.0.0.1:1122")
+			if err != nil {
+				b.Error(err)
+				return
+			}
+			defer conn.Close()
+
+			buf := make([]byte, batch)
+			for i := 0; i < count; i += batch {
+				_, err := conn.Write(buf)
+				if err != nil {
+					b.Error(err)
+					return
+				}
+				receive := batch
+				for receive > 0 {
+					n, err := conn.Read(buf)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+					receive -= n
+				}
+				if receive < 0 {
+					b.Errorf("received %d extra bytes", -receive)
+				}
+			}
+		}(count)
+	}
+	wg.Wait()
+}
+
+func BenchmarkFixedWorkers(b *testing.B) {
+	const batch = 64 * 1024
+
+	var (
+		wg sync.WaitGroup
+		ch = make(chan []byte)
+	)
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			conn, err := net.Dial("tcp", "127.0.0.1:1122")
+			if err != nil {
+				b.Error(err)
+				return
+			}
+			defer conn.Close()
+
+			buf := make([]byte, batch)
+			for val := range ch {
+				_, err = conn.Write(val)
+				if err != nil {
+					b.Error(err)
+					return
+				}
+
+				receive := len(val)
+				for receive > 0 {
+					n, err := conn.Read(buf)
+					if err != nil {
+						b.Error(err)
+						return
+					}
+					receive -= n
+				}
+				if receive < 0 {
+					b.Errorf("received %d extra bytes", -receive)
+				}
+			}
+		}()
+	}
+
+	for i := 0; i < b.N; i += batch {
+		ch <- make([]byte, batch)
+	}
+	close(ch)
+	wg.Wait()
+}
